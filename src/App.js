9a955d4193f0a20288ff915cd129e89ee10c845a
Alejandro Gonzalvez <alejandrogonzalvez@Alejandros-MacBook-Pro.local>
2021-09-03 13:18:37 -0400

    Header nav styling added.Used Router for Links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Header from './components/Header'
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Header />
     </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import SearchIcon from '@material-ui/icons/Search';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 
 
@@ -11,10 +13,12 @@ function Header() {
                 <input type='text' className='header__searchInput' />
                     <SearchIcon className='header__searchIcon' />
             </div>
+            <Link to='/' className='header__link'>  
             <div className='header__option'>
                 <span className='header__optionLineOne'>Hello, User</span>
                 <span className='header__optionLineTwo'>Sign In or Sing Out</span>
             </div>
+            </Link>
         </nav>
     )
 }
