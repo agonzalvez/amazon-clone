e62254f6583af4e5ec573b729decf160ba5ab75b
Alejandro Gonzalvez <alejandrogonzalvez@Alejandros-MacBook-Pro.local>
2021-09-03 13:52:39 -0400

    Added addtional link for Prime. Created login route. Added basket icon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import Header from './components/Header'
-import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/Header';
+import Login from './components/Login';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <Router>
     <div className="App">
-      <Header />
+      <Switch>
+        <Route path='/checkout'>
+        <Header />
+        </Route>
+        <Route path='/login'>
+        <Login />
+        </Route>
+      </Switch>
+      
     </div>
     </Router>
+
   );
 }
 
